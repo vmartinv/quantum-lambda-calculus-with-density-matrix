59fb13018a996cbe4f4756e03d68401cfd3b6849
Martín Villagra <vmartinv@fb.com>
2021-01-25 15:05:17 -0300

    cleanup code

--- src/compiler/app/Main.hs
+++ src/compiler/app/Main.hs
@@ -1,5 +1,30 @@
 module Main where
+
+import           Compiler
+import           Data.Semigroup      ((<>))
+import           Options.Applicative
 import           REPL
 
+
+data CmdFlags = CmdFlags
+  { interactive      :: Bool }
+
+cmdConfig :: Parser CmdFlags
+cmdConfig = CmdFlags
+      <$> switch
+          ( long "interactive"
+         <> short 'i'
+         <> help "Start an interpreter instance" )
+
+
 main :: IO ()
-main = repl
+main = work =<< execParser opts
+  where
+    opts = info (cmdConfig <**> helper)
+      ( fullDesc
+     <> progDesc "A compiler from \\lambda\\rho to Qiskit"
+     <> header "compiler" )
+
+work :: CmdFlags -> IO ()
+work (CmdFlags True) = repl
+work _               = getContents >>= print.compile
--- src/compiler/src/REPL.hs
+++ src/compiler/src/REPL.hs
@@ -17,35 +17,29 @@ cmd = liftIO.print.compile
 -- Tab Completion: return a completion for partial words entered
 completer :: Monad m => WordCompleter m
 completer n = do
-  let names = ["to", "do", "please", "complete"]
+  let names = ["letcase", "in"]
   return $ filter (isPrefixOf n) names
 
 -- Commands
 help :: [String] -> Repl ()
 help args = liftIO $ print $ "Help: " ++ show args
 
-say :: String -> Repl ()
-say args = do
-  _ <- liftIO $ callCommand $ "cowsay" ++ " " ++ args
-  return ()
-
 opts :: [(String, String -> Repl ())]
 opts =
-  [ ("help", help . words), -- :help
-    ("say", say) -- :say
+  [ ("help", help . words) -- :help
   ]
 
 ini :: Repl ()
-ini = liftIO $ putStrLn "Welcome!"
+ini = liftIO $ putStrLn "Welcome! Write \\lambda\\rho terms below"
 
 final :: Repl ExitDecision
 final = do
   liftIO $ putStrLn "Goodbye!"
   return Exit
 
-repl_alt :: IO ()
-repl_alt = evalReplOpts $ ReplOpts
-  { banner           = const $ pure ">>> "
+repl :: IO ()
+repl = evalReplOpts $ ReplOpts
+  { banner           = customBanner
   , command          = cmd
   , options          = opts
   , prefix           = Just ':'
@@ -55,9 +49,6 @@ repl_alt = evalReplOpts $ ReplOpts
   , finaliser        = final
   }
 
--- customBanner :: MultiLine -> Repl String
--- customBanner SingleLine = pure ">>> "
--- customBanner MultiLine  = pure "| "
-
-repl :: IO ()
-repl = evalRepl (const $ pure ">>> ") cmd opts (Just ':') (Just "paste") (Word0 completer) ini final
+customBanner :: MultiLine -> Repl String
+customBanner SingleLine = pure ">>> "
+customBanner MultiLine  = pure "| "
