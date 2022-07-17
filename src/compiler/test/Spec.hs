d975077ecba1ac4ff622fe4888041d6e6b2451e5
Martín Villagra <mvillagra0@gmail.com>
2022-07-17 13:38:30 +0100

    elimino codigo no usado

--- src/compiler/test/Spec.hs
+++ src/compiler/test/Spec.hs
@@ -2,7 +2,6 @@ import           ParserTests           (parserTests)
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Typing.QTypeTests     (qTypeTests)
-import           Typing.SmithTests     (smithTests)
 import           Typing.TypeCheckTests (typeCheckTests)
 
 main :: IO ()
