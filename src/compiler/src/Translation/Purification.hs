d84da57955d3ba383dbf34ce0ce3041e2591419f
Martín Villagra <mvillagra0@gmail.com>
2022-07-30 21:40:32 +0100

    limpio purification

--- src/compiler/src/Translation/Purification.hs
+++ src/compiler/src/Translation/Purification.hs
@@ -1,17 +1,17 @@
 module Translation.Purification where
 import           Data.Complex
-import qualified Numeric.LinearAlgebra.HMatrix as HMatrix
+import qualified Numeric.LinearAlgebra.HMatrix as HM
 import           Utils
 
 -- purifies a density matrix
 -- (assumes the input is a square matrix with a power of 2 size)
 -- given a matrix of 2^q x 2^q size, it returns a vector of 2^(q+1) size
 purify :: [[Double]] -> [Complex Double]
-purify mList = HMatrix.toList $ HMatrix.flatten $
-  sum (map f (zip3 [0..] (HMatrix.toList eigvals) (HMatrix.toRows eigvec)))
+purify mList = HM.toList $ HM.flatten $
+  sum (map f (zip3 [0..] (HM.toList eigvals) (HM.toRows eigvec)))
   where
-    m = HMatrix.fromLists (fmap (:+ 0.0) <$> mList)
-    (n, _) = HMatrix.size m
-    (eigvals, eigvec) = HMatrix.eig m
-    e i = HMatrix.asRow $ HMatrix.fromList $ [if i==j then (1.0 :+ 0.0) else (0.0 :+ 0.0) | j <- [0..n-1]] :: HMatrix.Matrix (Complex Double)
-    f (i, eigval, eigvec) = HMatrix.scale (sqrt eigval) (HMatrix.kronecker (HMatrix.asRow eigvec) (e i)) :: HMatrix.Matrix (Complex Double)
+    m = HM.fromLists (fmap (:+ 0.0) <$> mList)
+    (n, _) = HM.size m
+    (eigvals, eigvec) = HM.eig m
+    e i = HM.asRow $ HM.fromList $ [if i==j then (1.0 :+ 0.0) else (0.0 :+ 0.0) | j <- [0..n-1]] :: HM.Matrix (Complex Double)
+    f (i, eigval, eigvec) = HM.scale (sqrt eigval) (HM.kronecker (HM.asRow eigvec) (e i)) :: HM.Matrix (Complex Double)
